5cca6ccd73a58c90c41295b522c614dcba368b51
mikegladson <michaelgladson7@gmail.com>
2024-09-19 11:04:22 -0500

    removed tailwinds CSS header

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,7 +5,6 @@ import './index.css';
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello, Tailwind CSS!</h1>
       <PhotographyPortfolio />
     </div>
   );
