5c990bc0f65e067ca3b187756e2240eeef59e000
mikegladson <michaelgladson7@gmail.com>
2024-09-18 22:13:56 -0500

    updated h1 line of app.js

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,6 +5,7 @@ import './index.css';
 function App() {
   return (
     <div className="App">
+      <h1 className="text-3xl font-bold underline">Hello, Tailwind CSS!</h1>
       <PhotographyPortfolio />
     </div>
   );
